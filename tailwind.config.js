e0ad9d8a26aed4d56cc4c9d95e65a1fac128765a
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-11-04 16:09:33 +0100

    feat: added temp colors for light and dark mode, added html files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 export default {
   content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#000807',
+        inactive: '#a2a3bb',
+        primary: '#9395d3',
+        secondary: '#b3b7ee',
+        white: '#fbf9ff',
+        dark: {
+          inactive: '#6b6d88',
+          primary: '#4f4c7a',
+          secondary: '#7174a1',
+        }
+      },
+    },
   },
   plugins: [],
+  darkMode: 'selector'
 }
-
