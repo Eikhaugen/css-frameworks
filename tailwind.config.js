0d9d3b482f2eee7f9da3f5059eea9d5213395e3e
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-11-09 19:00:07 +0100

    feat: tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+  theme: {
+    extend: {
+      colors: {
+        
+      },
+      
+    },
+  },
+  darkMode: "class",
+  plugins: [] 
+}
