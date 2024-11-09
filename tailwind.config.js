897ad7752cde766a50e8d2c303236a1ab75e6dc4
thomas.eikhaugen@gmail.com <thomas.eikhaugen@gmail.com>
2024-11-09 21:24:29 +0100

    feat: added landingpage, login and registration page html and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+  content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
     extend: {
       colors: {
-        
+        primary: {
+          light: '#93c5fd', 
+          DEFAULT: '#3b82f6',
+          dark: '#1e40af', 
+        },
+        secondary: {
+          light: '#fda4af', 
+          DEFAULT: '#f43f5e',
+          dark: '#9f1239', 
+        },
+        accent: {
+          light: '#6ee7b7', 
+          DEFAULT: '#10b981',
+          dark: '#065f46', 
+        },
+        neutral: {
+          light: '#f3f4f6', 
+          DEFAULT: '#9ca3af',
+          dark: '#374151', 
+        },
+        warning: '#f59e0b', 
+        success: '#10b981', 
+        error: '#ef4444', 
       },
-      
     },
   },
   darkMode: "class",
-  plugins: [] 
-}
+  plugins: [],
+};
